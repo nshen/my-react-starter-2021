274c395257488e7a45654ad8f9c52922f3bc1631
Xin.Chen <xin.chen@percent.cn>
2021-07-12 18:18:06 +0800

    feat: add axios-hooks

--- pages/dynamic-demo.tsx
+++ pages/dynamic-demo.tsx
@@ -1,28 +1,33 @@
 import React, { useEffect, useState } from "react";
+import useAxios from "axios-hooks";
 import ArticleList from "../components/ArticleList";
+import { Button, Flex } from "@chakra-ui/react";
 
 type ArticleType = { title: string };
 const DynamicDemo = () => {
-  const [articles, setArticles] = useState<ArticleType[]>([]);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setArticles([
-        { title: "动态加载数据" },
-        { title: "test 22222222222" },
-        { title: "test 333" },
-      ]);
-      return () => {
-        clearTimeout(timer);
-      };
-    }, 1000);
-  }, []);
-  return articles.length > 0 ? (
-    <div>
-      <ArticleList articles={articles} />
-    </div>
-  ) : (
-    <div>loading</div>
+  const [{ data, loading, error }, refetch] = useAxios(
+    "https://jsonplaceholder.typicode.com/posts"
   );
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+  if (data) {
+    return (
+      <Flex direction="column" justify="flex-start" my="3">
+        <ArticleList
+          articles={data.map((article: any) => ({
+            title: article.title,
+          }))}
+        />
+        <Button
+          onClick={() => {
+            refetch();
+          }}
+        >
+          refetch
+        </Button>
+      </Flex>
+    );
+  }
 };
 
 export default DynamicDemo;
