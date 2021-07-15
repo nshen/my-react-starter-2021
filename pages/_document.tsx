69a900e06682f949388b148006a3da04a61eefab
Xin.Chen <xin.chen@percent.cn>
2021-07-15 10:01:46 +0800

    fix(_document.tsx): remove html lang attribute

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,12 @@
+// 使用 chakra-ui color mode 需要扩展此类
+// https://chakra-ui.com/docs/features/color-mode
 import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import theme from "../styles/theme";
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html lang="zh-CN">
+      <Html>
         <Head />
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
