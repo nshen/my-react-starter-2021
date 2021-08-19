bf48c9e4441fc6245b025008bb48042af4edf1d3
Xin.Chen <xin.chen@percent.cn>
2021-08-19 09:37:13 +0800

    feat: add useDebounce hook

---
+++ buildin/hooks/useDebounce.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+
+function useDebounce<T>(value: T, delay?: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export default useDebounce;
+
+// from https://usehooks-typescript.com/react-hook/use-debounce
+
+/** 
+ * useage
+ * 
+ import React, { ChangeEvent, useEffect, useState } from 'react'
+
+import useDebounce from './useDebounce'
+
+export default function Component() {
+  const [value, setValue] = useState<string>('')
+  const debouncedValue = useDebounce<string>(value, 500)
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  }
+
+  // Fetch API (optional)
+  useEffect(() => {
+    // Do fetch here...
+    // Triggers when "debouncedValue" changes
+  }, [debouncedValue])
+
+  return (
+    <div>
+      <p>Value real-time: {value}</p>
+      <p>Debounced value: {debouncedValue}</p>
+
+      <input type="text" value={value} onChange={handleChange} />
+    </div>
+  )
+}
+*/
