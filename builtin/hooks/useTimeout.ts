46307b1ae687e8a9b32c1752a3cf4d5f8c0a4aef
Xin.Chen <xin.chen@percent.cn>
2021-08-20 12:04:15 +0800

    feat: add useTimeout hook

---
+++ builtin/hooks/useTimeout.ts
@@ -0,0 +1,50 @@
+import { useEffect, useRef } from 'react';
+
+function useTimeout(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback);
+
+  // Remember the latest callback if it changes.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the timeout.
+  useEffect(() => {
+    // Don't schedule if no delay is specified.
+    if (delay === null) {
+      return;
+    }
+
+    const id = setTimeout(() => savedCallback.current(), delay);
+
+    return () => clearTimeout(id);
+  }, [delay]);
+}
+
+export default useTimeout;
+
+// from: https://usehooks-typescript.com/react-hook/use-timeout
+
+// usage:
+
+// import React, { useState } from 'react'
+
+// import useTimeout from './useTimeout'
+
+// export default function Component() {
+//   const [visible, setVisible] = useState(true)
+
+//   const hide = () => setVisible(false)
+
+//   useTimeout(hide, 5000)
+
+//   return (
+//     <div>
+//       <p>
+//         {visible
+//           ? "I'm visible for 5000ms"
+//           : 'You can no longer see this content'}
+//       </p>
+//     </div>
+//   )
+// }
